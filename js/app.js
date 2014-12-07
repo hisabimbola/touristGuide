adee56a4b4c124f77463999ce62642bdf047319b
Abimbola Idowu <abimbola2010@gmail.com>
2014-12-07 11:12:55 +0100

    Image now displaying for each tourist attraction that has image link

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
 var touristGuide = {
 
   searchValue: null,
+  topicHTML: "",
+  baseUrl: "https://www.freebase.com/",
   serviceUrl: 'https://www.googleapis.com/freebase/v1/search', //base endpoint for the search service
   topicUrl: 'https://www.googleapis.com/freebase/v1/topic', //base endpoint for the search service
 
   init: function() {
     this.submitSearch();
-    //console.log('init!!!');
   },
 
   //params in method, because searchValue depends on search from user
   params: function () {
-    //id: "/travel/tourist_attraction"
    return { "domain": "travel",
     'query': this.searchValue,
     'type': '/travel/tourist_attraction',
-    //"filter": "(all type:/location/country/name)",
     'indent': true //for nice indented JSON format, needed for development only
     };
   },
@@ -34,26 +33,32 @@ var touristGuide = {
   //makes json call
   getJson: function () {
     $.getJSON(this.serviceUrl + '?callback=?', this.params(), function(topic) {
-    //console.log(topic);
-    $.each(topic.result, function(index, val) {
-       /* iterate through array or object */
-       //console.log(val.id || "none");
-       touristGuide.getTopic(val.id);
-       //$('<div>', {text:val.id.text});
-
+      var imageUrl = "";
+      $.each(topic.result, function(i, val) {
+        //  /* iterate through array or object */
+        touristGuide.getTopic(val.id);
+      });
     });
-  });
   },
 
-  //sends empty params, name will still change
-  paras: {
-
-  },
   //method to get topic
   getTopic: function (topicId) {
-    $.getJSON(this.topicUrl + topicId + '?callback=?', this.paras, function (topic) {
-      console.log(topic);
+    $.getJSON(this.topicUrl + topicId + '?callback=?', null, function (topic) {
+      if (topic.property['/common/topic/image']) {
+        imageUrl = topic.property['/common/topic/image'].values[0].id;
+      }      
+        // console.log(imageUrl);
+        touristGuide.topicHTML = '<li> <img src="https://usercontent.googleapis.com/freebase/v1/image' + imageUrl + '"> </li>';
+        console.log(touristGuide.topicHTML);
+        touristGuide.showTopic(touristGuide.topicHTML);
+         // console.log({text:value['/type/object/name']});
+         // console.log(val['/common/topic/image'].count);
+      //    $('<div>',{text:val['/common/topic/image'].values[0].text}).appendTo($('view'));
     });
+  },
+
+  showTopic: function (display) {
+    $('#display').append(display);
   }
 };
 
